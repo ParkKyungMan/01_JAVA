313cb5c4e3a4f6966ac2f5e0ea4a54ff967e2726
ParkKyungMan <park870505@gmail.com>
2024-08-26 15:19:52 +0900

    [240826 수업자료]

---
+++ chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.test;
+
+public class Application {
+
+    // 한 줄 주석
+    // 필기를해보아요
+
+    // 여러 줄 주석 슬레시 별별 슬레시
+    /*ㅏㅐㅈ라ㅐㅈ람랄
+    * 자ㅐㄹ마라매라맮
+    * 매람재라
+    /*수업목표
+   /* 필기
+   main 영역은 프로그램을 동작시킬 수 있는 영역이다.*/
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("안녕하세요");
+        System.out.println("문정현입니다");
+        System.out.println("배가고픕니다");
+        System.out.println('굿');
+        System.out.println("my name is jeonghyeonmoon");
+     //   System.out.println(안녕하세요);
+     }
+
+
+}
---
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section02/variable/Application1.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section02.variable;
+
+public class Application1 {
+
+
+    public static void main(String[] args) {
+
+
+        /* 목차. 3. 한 번 저장해둔 닶을 재사용하기 위한 목적  */
+        int point = 100;
+
+
+        System.out.println("===============변수에 저장한 값 재사용 테스트===================");
+        // 10명의 고객에게 100 포인트 지급하는 출력문을 만들어보자.
+        System.out.println("1번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("2번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("3번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("4번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("5번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("6번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("7번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("8번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("9번 고객에게 " + point + "포인트 지급하였습니다.");
+        System.out.println("10번 고객에게 " + point + "포인트 지급하였습니다.");
+
+        /* 목차. 3. 시간에 따라 변경되는 값을 저장하고 사용할 수 있다.  */
+        System.out.println("===============변수에 저장 된 값 변경 테스트===========");
+        int sum = 0;
+        // soutv   :    가까운 변수를 출력해주는 기능
+        System.out.println("sum = " + sum);
+
+        sum = sum + 10;
+        System.out.println("sum 에 10을 더한 2번째 sum = " + sum);
+
+        sum = sum + 10;
+        System.out.println("sum에 10을 더한 3번 째 sum = " + sum);
+
+    }
+
+
+}
